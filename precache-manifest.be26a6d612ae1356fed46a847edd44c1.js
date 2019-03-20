ecd9372c7b5c25cd11694919cc4b97c52fff2799
Van Boxsel Engineering <accounts@vanboxsel.nl>
2019-03-20 13:46:09 +0100

    Updates

---
+++ precache-manifest.be26a6d612ae1356fed46a847edd44c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfdeac7b708fc05f1576d456b8abd28d",
+    "url": "maartenbreeedveld.github.io/breeed.com/static/media/kast_img.dfdeac7b.jpg"
+  },
+  {
+    "revision": "a71c8d77f4942750af7c",
+    "url": "maartenbreeedveld.github.io/breeed.com/static/js/runtime~main.03bdeea0.js"
+  },
+  {
+    "revision": "b485aff716a20fd6c6c6",
+    "url": "maartenbreeedveld.github.io/breeed.com/static/js/main.208ce0dc.chunk.js"
+  },
+  {
+    "revision": "815e1c550e5717fa09ba",
+    "url": "maartenbreeedveld.github.io/breeed.com/static/js/2.0ee0d8c1.chunk.js"
+  },
+  {
+    "revision": "b485aff716a20fd6c6c6",
+    "url": "maartenbreeedveld.github.io/breeed.com/static/css/main.b3c71ec4.chunk.css"
+  },
+  {
+    "revision": "ef441d01a6becb71316dec0903cfac5f",
+    "url": "maartenbreeedveld.github.io/breeed.com/index.html"
+  }
+];
